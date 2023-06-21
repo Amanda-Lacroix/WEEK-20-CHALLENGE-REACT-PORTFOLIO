b7068fbc4ac2e2d009b056d6d5fe7ba5448007bf
Beezyz <116973964+Beezyz@users.noreply.github.com>
2023-06-21 18:17:10 -0300

    revised icons in footer

--- react-portfolio/src/components/Footer.js
+++ react-portfolio/src/components/Footer.js
@@ -12,10 +12,11 @@ const Footer = () => {
     <div class="social-container" className="box-style" id="contact">
       <a href="https://www.instagram.com/feet_heartbeat_will_travel"
         className="instagram social">
-        <FontAwesomeIcon icon={faInstagram} size="2x" />
-        {/* <FontAwesomeIcon icon={faGithub} size="2x" /> */}
-
-      </a>
+        <FontAwesomeIcon icon={faInstagram} size="2x" /></a>
+        <a href="https://github.com/Amanda-Lacroix"
+        className="github">
+        <FontAwesomeIcon icon={faGithub} size="2x" />
+          </a>
     </div>
   );
 };
