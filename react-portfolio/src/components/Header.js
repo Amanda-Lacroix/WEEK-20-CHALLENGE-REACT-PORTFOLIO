d94196c6ae338e57c7a2a65197f97a79d8b48c10
Beezyz <116973964+Beezyz@users.noreply.github.com>
2023-06-21 16:59:47 -0300

    rendering an images for profile picture

--- react-portfolio/src/components/Header.js
+++ react-portfolio/src/components/Header.js
@@ -5,7 +5,7 @@ import Navigation from './Navigation';
 const Header = ({ handleNavLinkClick }) => {
   return (
     <header className="box-style">
-      <img id="Amanda" src="./Assets/AmandA.jpg" alt="Woman wearing a leopard print coat, flashing the peace sign with a sunset in the background." />
+      <img className="profile-photo" src={require("../Images/")} alt={"Woman wearing a leopard print coat, flashing the peace sign with a sunset in the background"}/>
       <h1 className="font-effect-shadow-multiple">Portfolio of Amanda Lacroix</h1>
       {/* Render the Navigation component and pass the handleNavLinkClick function as a prop */}
       <Navigation handleNavLinkClick={handleNavLinkClick} />
