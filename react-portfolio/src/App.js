bd34e9a9c5c00db8e8998932dd2cbda2f1fa0d34
Beezyz <116973964+Beezyz@users.noreply.github.com>
2023-06-21 15:48:07 -0300

    revised Navigation and App.js for click eventhandling--troubleshooting rendering & handling

--- react-portfolio/src/App.js
+++ react-portfolio/src/App.js
@@ -1,11 +1,7 @@
-import React, { useState } from 'react';
+import React from 'react';
 import Header from './components/Header';
-import About from './components/About';
 import Footer from './components/Footer';
-import Navigation from './components/Navigation';
-import Projects from './components/Projects';
-import Resume from './components/Resume';
-import Contact from './components/Contact';
+
 
 
 import './style.css';
@@ -13,31 +9,9 @@ import './style.css';
 
 
 function App() {
-  const [activeSection, setActiveSection] = useState('about');
-
-  const handleNavLinkClick = (section) => {
-    setActiveSection(section);
-  };
-
-  function renderSection() {
-    switch (activeSection) {
-      case 'about':
-        return <About />;
-      case 'projects':
-        return <Projects />;
-      case 'resume':
-        return <Resume />;
-      case 'contact':
-        return <Contact />;
-      default:
-        return null;
-    }
-  }
-
   return (
     <div className="App">
       <Header />
-      <Navigation onNavLinkClick={handleNavLinkClick} activeSection={activeSection} />
       <main>
       {renderSection()}
       </main>
--- react-portfolio/src/components/Navigation.js
+++ react-portfolio/src/components/Navigation.js
@@ -1,14 +1,45 @@
-import React from 'react';
-
+import React, { useState } from 'react';
+import About from './About';
+import Projects from './Projects';
+import Resume from './Resume';
+import Contact from './Contact';
 
 const Navigation = () => {
+  const [activeSection, setActiveSection] = useState('about');
+
+  const handleNavLinkClick = (section) => {
+    setActiveSection(section);
+  };
+
+  function renderSection() {
+    switch (activeSection) {
+      case 'about':
+        return <About />;
+      case 'projects':
+        return <Projects />;
+      case 'resume':
+        return <Resume />;
+      case 'contact':
+        return <Contact />;
+      default:
+        return null;
+    }
+  }
   return (
     <nav className="navigation">
       <ul>
-        <li><a href="#contact">CONTACT</a></li>
-        <li><a href="#resume">RESUME</a></li>
-        <li><a href="#projects">PORTFOLIO</a></li>
-        <li><a href="#about">ABOUT</a></li>
+      <li>
+  <a href="#contact" onClick={() => handleNavLinkClick('contact')}>CONTACT</a>
+</li>
+<li>
+  <a href="#resume" onClick={() => handleNavLinkClick('resume')}>RESUME</a>
+</li>
+<li>
+  <a href="#projects" onClick={() => handleNavLinkClick('projects')}>PORTFOLIO</a>
+</li>
+<li>
+  <a href="#about" onClick={() => handleNavLinkClick('about')}>ABOUT</a>
+</li>
       </ul>
     </nav>
   );
