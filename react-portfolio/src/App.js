35689459cc88886ca8cb7b6ed7098a325a59f91e
Beezyz <116973964+Beezyz@users.noreply.github.com>
2023-06-20 22:01:08 -0300

    added code to About.js, moved css and Assets folder into src folder, added code to App.js

--- react-portfolio/src/App.js
+++ react-portfolio/src/App.js
@@ -6,7 +6,7 @@ import Projects from './components/Projects';
 import Contact from './components/Contact';
 import Footer from './components/Footer';
 
-// import CSS
+import './style.css';
 
 function App() {
   return (
---
+++ react-portfolio/src/components/About.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+// import ''
+
+const Footer = () => {
+  return (
+    <footer className="box-style">
+      <h2>CONTACT</h2>
+      <a href="https://github.com/amanda-lacroix"><span>GitHub</span></a>
+      <a href="https://linkedin.com/in/amanda-lacroix"><span>LinkedIn</span></a>
+    </footer>
+  );
+};
+
+export default Footer;
